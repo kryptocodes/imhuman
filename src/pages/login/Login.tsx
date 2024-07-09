5b295e0dbcac6cca7e5b6db75e667f0e72fb1246
kryptocodes <srivatsantb@gmail.com>
2024-07-09 20:52:39 +0530

    feat: add verify btn
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -16,7 +16,6 @@ const Login = () => {
 
   useEffect(() => {
     if (account.isConnected) {
-      handleConnect(account.address as string)
       toast('Connected Successfully')
       // navigate('/')
     }
@@ -41,6 +40,9 @@ const Login = () => {
   };
 
   const handleConnect = async ( walletAddress:string ) =>{
+    if(!walletAddress){
+      toast('Connecting')
+    }
     const nonceData:{nonce:string,id:string} = await generateNonce(walletAddress);
 
     const message = await signMessageAsync({message:nonceData.nonce});
@@ -89,11 +91,18 @@ const Login = () => {
         <p className=' font-neueBit leading-none text-[64px]  translate-y-2 z-[100000] ' >Ditch the Bots</p>
         <p className=' font-eiko text-6xl  textBorder leading-[57px] -translate-y-3  z-[1000] '>Embrace <br /> Humanity</p>
       </div>
-      <div className="bg-white rounded-2xl px-[2px] mt-11 pt-[2px] pb-[6px] ">
-        {/* <button className=" text-brand border-2 font-bold py-2 px-9 rounded-2xl border-brand" >
-          Connect Wallet
-        </button> */}
-        <ConnectButton/>
+      <div
+    
+      className="bg-white rounded-2xl px-[2px] mt-11 pt-[2px] pb-[6px] ">
+        {account?.isConnected ? 
+        <button 
+          onClick={() => handleConnect(account?.address as string)}
+        className=" text-brand border-2 font-bold py-2 px-9 rounded-2xl border-brand" >
+          Verify
+        </button> :
+        <ConnectButton /> 
+      }
+      
       </div>
       <div className="relative w-full flex mt-16  justify-center items-center ">
         <img loading="eager" src="/human.svg" className=" w-[250px] " alt="" />
